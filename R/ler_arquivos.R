02a538b6739a1fcdbbafc037b89eb4f4f3c76a63
Winicius Faquieri <54724688+wfaquieri@users.noreply.github.com>
2022-08-24 19:23:34 -0300

    string ascii

--- R/ler_arquivos.R
+++ R/ler_arquivos.R
@@ -1,6 +1,6 @@
 #' ler_arquivos
 #'
-#' Recebe os caminhos (paths) dos arquivos no dir e o número de sheet como 
+#' Recebe os caminhos (paths) dos arquivos no dir e o número de sheet como
 #' parâmetro e retorna uma tibble arrumada.
 #'
 #' @param x lista de string e uma constante.
@@ -8,37 +8,69 @@
 #' @export
 
 # Define a função de leitura dos arquivos em .xlsx
-ler_arquivos = 
-  function(paths,n_sheet) {
-    purrr::map(1:n_sheet,
-               ~ readxl::read_xlsx(
-                 paths, 
-                 sheet = .x, 
-                 range = readxl::cell_cols("A:T"),
-                 col_types = c("text", "date","text","text", "text",
-                               "text", "text","text","text", "text",
-                               "text", "text","text","text", "text",
-                               "text", "text","text","date", "text")
-               ) |> dplyr::mutate(Origem = arrumar_origem(paths)) |> 
-                 dplyr::select(
-                   Origem,
-                   Analista = 1,
-                   Data = 2,
-                   Job = 3,
-                   Elementar = 4,
-                   Familia = 5,
-                   UF_Preco = 9,
-                   `Abert/Ampl` = 10,
-                   BP = 11,
-                   UF_Escritorio = 12,
-                   Coletor = 13,
-                   Empresa = 14,
-                   CNPJ = 15,
-                   Status = 18,
-                   Retorno = 19,
-                   Obs. = 20
-                 ) |> 
-                 dplyr::mutate(
-                   Obs. = stringi::stri_trans_general(str = Obs., id = "Latin-ASCII"))
+ler_arquivos =
+  function(paths, n_sheet) {
+    purrr::map(
+      1:n_sheet,
+      ~ readxl::read_xlsx(
+        paths,
+        sheet = .x,
+        range = readxl::cell_cols("A:T"),
+        col_types = c(
+          "text",
+          "date",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "text",
+          "date",
+          "text"
+        )
+      ) |> dplyr::mutate(Origem = arrumar_origem(paths)) |>
+        dplyr::select(
+          Origem,
+          Analista = 1,
+          Data = 2,
+          Job = 3,
+          Elementar = 4,
+          Familia = 5,
+          UF_Preco = 9,
+          `Abert/Ampl` = 10,
+          BP = 11,
+          UF_Escritorio = 12,
+          Coletor = 13,
+          Empresa = 14,
+          CNPJ = 15,
+          Status = 18,
+          Retorno = 19,
+          Obs. = 20
+        ) |>
+        dplyr::mutate(
+          Obs. = stringi::stri_trans_general(str = Obs., id = "Latin-ASCII"),
+          Origem = stringi::stri_trans_general(str = Origem, id = "Latin-ASCII"),
+          Familia = stringi::stri_trans_general(str = Familia, id = "Latin-ASCII"),
+          Empresa = stringi::stri_trans_general(str = Empresa, id = "Latin-ASCII"),
+          Analista = stringi::stri_trans_general(str = Analista, id = "Latin-ASCII"),
+          Job = stringi::stri_trans_general(str = Job, id = "Latin-ASCII"),
+          Elementar = stringi::stri_trans_general(str = Elementar, id = "Latin-ASCII"),
+          Empresa = stringi::stri_trans_general(str = Empresa, id = "Latin-ASCII"),
+          UF_Preco = stringi::stri_trans_general(str = UF_Preco, id = "Latin-ASCII"),
+          UF_Escritorio = stringi::stri_trans_general(str = UF_Escritorio, id = "Latin-ASCII"),
+          Coletor = stringi::stri_trans_general(str = Coletor, id = "Latin-ASCII"),
+          Coletor = stringi::stri_trans_general(str = Coletor, id = "Latin-ASCII"),
+          `Abert/Ampl` = stringi::stri_trans_general(str = `Abert/Ampl`, id = "Latin-ASCII")
+        )
     )
   }
